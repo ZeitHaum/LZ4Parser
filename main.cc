7a830e1b20bb0542c3cdc92e3b0d63364c953b27
ZeitHaum <zeithaum@qq.com>
2024-05-13 01:40:51 +0800

    Complete Basic Parser

--- main.cc
+++ main.cc
@@ -33,13 +33,156 @@ void Parser::init(const std::string& file_name){
     std::cout<<"INFO:" << "init input files, read data Bytes:" << file_data.size << "\n";
 }
 
+DataBlockInfo::DataBlockInfo(){
+    memset(this, 0, sizeof(DataBlockInfo));
+}
+
+std::string Parser::restore_data(const std::string& origin_str, uint16_t offset, int match_length){
+    //check
+    assert(offset<=origin_str.length());
+    assert(match_length<=(origin_str.length()-offset+1));
+    std::string sub_str = origin_str.substr(origin_str.length() - offset, match_length);
+    return sub_str;
+}
+
 //Core Function.
 void Parser::parse_and_decompress(const std::string& dep_file_name){
-
+    std::ofstream dep_file = std::ofstream(dep_file_name);
+    uint8_t* scan_ptr = ptr;
+    //parse Magic Number.
+    get_uInt32F(magic_num);
+    assert(magic_num==0x184D2204);
+    log_parsed(magic_num);
+    log_parse_ok("Magic Number");
+    //parse File descriptor
+    get_byteF(flag);
+    get_byteF(BDbyte);
+    if(testbit_byte(flag, 3)){
+        get_uInt64F(content_size);
+        log_parsed(content_size);
+    }
+    if(testbit_byte(flag, 0)){
+        get_uInt32F(dic_id);
+        log_parsed(dic_id);
+    }
+    get_byteF(header_checksum);
+    log_parsed((int)header_checksum);
+    log_parse_ok("File descriptor");
+    /*
+     parse DataBlock.
+    */
+    std::string dep_block_str;
+    auto parse_token = [&](){
+        //get literals length
+        get_byteD(token);
+        uint8_t prev_literals_length = get_high_nibble(token);
+        int total_literals_length = (int)(prev_literals_length);
+        if(prev_literals_length==0x0F){
+            while(1){
+                get_byteD(tmp_byte);
+                total_literals_length+=(int)tmp_byte;
+                if(tmp_byte<0xFF) break;
+            }
+        }
+        log_parsed(total_literals_length);
+        std::string literals;
+        for(int i = 0;i<total_literals_length; ++i){
+            get_byteD(tmp_literals);
+            literals.push_back((char)(tmp_literals));
+        }
+        dep_block_str+=literals;
+        log_parsed(literals);
+        //get offset
+        get_uInt16D(offset);
+        log_parsed(offset);
+        assert(offset<=0xFFFF);
+        //get match lenghth.
+        uint8_t prev_match_length = get_low_nibble(token);
+        int total_match_length = (int)(prev_match_length);
+        if(prev_match_length==0x0F){
+            while(1){
+                get_byteD(tmp_byte);
+                total_match_length+=(int)tmp_byte;
+                if(tmp_byte<0xFF) break;
+            }
+        }
+        total_match_length+=4;// more 4 byte is enabled.
+        log_parsed(total_match_length);
+        std::string comp_str =  restore_data(dep_block_str, offset, total_match_length);
+        dep_block_str+=comp_str;
+        std::string dep_token_str=literals+comp_str;
+        log_parsed(dep_token_str);
+    };
+    while(1){
+        //get BlockSize
+        get_uInt32D(block_size_data);
+        if(block_size_data==0x0){
+            std::cout<<"INFO: End mark find.\n";
+            break;
+        }
+        uint32_t _split_num = 0x80000000;
+        uint32_t block_size = block_size_data;
+        bool is_compressed = true;
+        if(block_size_data>_split_num){
+            //uncompressed
+            block_size -= _split_num;
+            is_compressed = false;
+        }
+        log_parsed(block_size);
+        log_parsed(is_compressed);
+        //ParseData
+        DataBlockInfo data_block_info;
+        dep_block_str.clear();
+        if(!is_compressed){
+            data_block_info.is_compressed = false;
+            data_block_info.origin_size=block_size;
+            data_block_info.is_valid = true;
+            data_block_info.actual_size=4+block_size;
+            for(int i = 0; i<block_size; ++i){
+                get_byteD(tmp_byte);
+                dep_block_str.push_back((char)tmp_byte);
+            }
+            log_parsed(dep_block_str);
+            dep_file<<dep_block_str;
+        }
+        else{
+            //parse tockens.
+            uint32_t now_dep_len = 0;
+            while(1){
+                int begin_sz = parse_stat.data_blocks_size;
+                parse_token();
+                log_parse_ok("One Token");
+                now_dep_len += (uint32_t)(parse_stat.data_blocks_size - begin_sz);
+                if(now_dep_len>=block_size){
+                    break;
+                }
+            }
+        }
+        dep_file<< dep_block_str;
+        log_parse_ok("One Block");
+        if(testbit_byte(flag, 4)){
+            get_uInt32D(block_checksum);
+            log_parsed(block_checksum);
+        }
+        break;
+    }
+    //Parse CheckSum.
+    if(testbit_byte(flag, 2)){
+        get_uInt32F(content_check_sum);
+        log_parsed(content_check_sum);
+    }
+    dep_file.close();
+    get_uInt16(extra_bytes);
+    std::cout<<extra_bytes<<"\n";
+    assert(scan_ptr-file_data.data_ptr==file_data.size);
+    assert(parse_stat.data_blocks_size + parse_stat.frame_size==file_data.size);
+    log_parse_ok("Parse Succeccfully ended.");
 }
 
 void Parser::dump_stat(){
-
+    std::cout<<"Stat:"<<"frame_size is " << parse_stat.frame_size <<".\n";
+    std::cout<<"Stat:"<<"data_blocks_size is " << parse_stat.data_blocks_size <<".\n";
+    //Dump dataBlock infos.
 }
 
 int main(){
@@ -48,5 +191,6 @@ int main(){
     Parser parser;
     parser.init(file_name);
     parser.parse_and_decompress(dep_file_name);
+    parser.dump_stat();
     return 0;
 }
--- main.hh
+++ main.hh
@@ -2,6 +2,28 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <cassert>
+#include <cstring>
+
+//define region
+#define ptr file_data.data_ptr
+#define get_byte(var) uint8_t var=*(scan_ptr);++scan_ptr
+#define get_uInt16(var) uint16_t var=*((uint16_t*)(scan_ptr));scan_ptr+=2
+#define get_uInt32(var) uint32_t var=*((uint32_t*)(scan_ptr));scan_ptr+=4
+#define get_uInt64(var) uint64_t var=*((uint64_t*)(scan_ptr));scan_ptr+=8
+#define get_byteF(var) get_byte(var);parse_stat.frame_size+=1
+#define get_uInt16F(var) get_uInt16(var);parse_stat.frame_size+=2
+#define get_uInt32F(var) get_uInt32(var);parse_stat.frame_size+=4
+#define get_uInt64F(var) get_uInt64(var);parse_stat.frame_size+=8
+#define get_byteD(var) get_byte(var);parse_stat.data_blocks_size+=1
+#define get_uInt16D(var) get_uInt16(var);parse_stat.data_blocks_size+=2
+#define get_uInt32D(var) get_uInt32(var);parse_stat.data_blocks_size+=4
+#define get_uInt64D(var) get_uInt64(var);parse_stat.data_blocks_size+=8
+#define testbit_byte(byte, i) ((byte>>i)&0x01)
+#define get_high_nibble(byte) ((byte>>4)&0x0F)
+#define get_low_nibble(byte) (byte&0x0F)
+#define log_parsed(var)  std::cout<<"INFO:"<<"Parsed variable "#var" "<<var<<".\n"
+#define log_parse_ok(str) std::cout<<"INFO: [OK] "<<"Parsed "<<str<<".\n";
 
 struct FileData{
     uint8_t* data_ptr = nullptr;
@@ -10,16 +32,14 @@ struct FileData{
     void clear();
 };
 
-enum class DataBlockType{
-    DATA, REFERENCE,
-};
-
-struct DataBlock{
-    DataBlockType data_block_type;
+struct DataBlockInfo{
+    bool is_valid;
+    bool is_compressed;
     uint64_t actual_size;
     uint64_t origin_size;
     uint64_t ref_offeset;
     uint64_t ref_length;
+    DataBlockInfo();
 };
 
 class Parser{
@@ -28,9 +48,9 @@ private:
     struct {
         int frame_size;
         int data_blocks_size;
-        std::vector<DataBlock> data_blocks;
+        std::vector<DataBlockInfo> data_blocks;
     } parse_stat;
-    std::ofstream dep_file;
+    std::string restore_data(const std::string& origin_str, uint16_t offset, int match_length);
 public:
     ~Parser();
     void parse_and_decompress(const std::string& dep_file_name);
